84fa89024982a26235989111d02b8e78a0972ba0
Andrew Wilson <adwilson12901@gmail.com>
2023-11-05 15:45:35 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(action = 'roller-skate') {
+    return `This Saturday, I want to ${action}!`
+}
+
+saturdayFun()
+
+function mondayWork(location = 'go to the office') {
+    return `This Monday, I will ${location}.`
+}
+
+mondayWork()
+
+// const starHighlight = wrapAdjective('*')
+// const starCompliment = 'a hard worker'
+// const lineHighlight = wrapAdjective('||')
+// const lineCompliment = 'a dedicated programmer'
+
+
+function wrapAdjective(highlight = 'special') {
+    return function (adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
